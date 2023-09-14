61248f8efc0ed520a1017134ed4651911e475d0e
GershonSolomon <144811765+GershonSolomon@users.noreply.github.com>
2023-09-14 10:12:36 +0530

    Add files via upload

---
+++ JDBCDemo.java
@@ -0,0 +1,37 @@
+package jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+ 
+class JDBCDemo {
+    public static void main(String[] args) throws Exception
+    {
+    	String jdbcUrl = "jdbc:mysql://localhost:3306/day2";
+        String username = "root";
+        String password = "Gero@2002";
+        try {
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+           
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            
+            Statement statement = connection.createStatement();
+            
+            String sql = "SELECT * FROM entry";
+            ResultSet resultSet = statement.executeQuery(sql);
+            
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ID");
+                String name = resultSet.getString("NAME");
+                System.out.println("ID: " + id + ", Name: " + name);
+            }
+            
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
